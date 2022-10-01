2c13e4a3de0c9dc19c8ed9ad66ee0f0a94c955a8
Guy Chamberlain-Webber <chamberlainwebberj@gmail.com>
2022-10-01 19:18:21 +0100

    Created the reload radial progess indicator

---
+++ Assets/Scripts/ReloadProgressIndicator.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ReloadProgressIndicator : MonoBehaviour
+{
+    private float _startReloadTime;
+    private float _reloadTime;
+
+    private bool _isProgressing;
+
+    private Image _reloadProgressIndicator;
+
+    private PlayerReloadEvent _reloadEvent;
+    private void Start()
+    {
+        EventManager.AddListener<PlayerReloadEvent>(OnReload);
+
+        _reloadProgressIndicator = GetComponent<Image>();
+        _reloadProgressIndicator.enabled = false;
+    }
+
+    private void OnReload(PlayerReloadEvent evt)
+    {
+        _reloadEvent = evt;
+
+        _startReloadTime = Time.time;
+        _reloadTime = _reloadEvent.reloadTime;
+
+        _reloadProgressIndicator.enabled = true;
+
+        _isProgressing = true;
+    }
+
+    private void Update()
+    {
+        float timeSinceReloadStarted = Time.time - _startReloadTime;
+
+        if (_isProgressing)
+        {
+            if (timeSinceReloadStarted < _reloadTime)
+            {
+                _reloadProgressIndicator.fillAmount = (timeSinceReloadStarted / _reloadTime);
+            }
+            else
+            {
+                _reloadProgressIndicator.fillAmount = 0;
+
+                _isProgressing = false;
+
+                _reloadEvent.onReloadInstance.Invoke();
+            }
+        }
+        else
+        {
+            _reloadProgressIndicator.enabled = false;
+        }
+    }
+}

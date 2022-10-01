67d6d9e6fa03331d26479c97dfa95eab5b192ad5
Guy Chamberlain-Webber <chamberlainwebberj@gmail.com>
2022-10-01 22:30:10 +0100

    Added move sophisticated camera tracking

---
+++ Assets/Scripts/FollowBehaviour.cs
@@ -0,0 +1,53 @@
+﻿using System.Linq;
+using UnityEngine;
+
+public class FollowBehaviour : MonoBehaviour
+{
+    [SerializeField] private float _minHeight;
+    [SerializeField] private float _maxHeight;
+
+    [SerializeField] private float _offsetX;
+    [SerializeField] private float _offsetY;
+
+    [SerializeField] private float _lookAheadFactor = 3;
+    [SerializeField] private float _damping;
+
+    [SerializeField] protected Transform trackingTarget;
+
+    private Vector2 _lastTargetPosition;
+    private Vector2 _currentVelocity;
+    private Vector2 _lookAheadPosition;
+
+    private Player _player;
+
+    private void Start()
+    {
+        _lastTargetPosition = trackingTarget.position;
+
+        _player = FindObjectOfType<Player>();
+    }
+
+    private void Update()
+    {
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 moveDelta = mousePosition - _lastTargetPosition;
+
+        bool updateLookAheadFactor = moveDelta.x > 0 || moveDelta.y > 0;
+
+        if (updateLookAheadFactor)
+        {
+            float[] deltas;
+            deltas = new float[] { moveDelta.x, moveDelta.y };
+
+            _lookAheadPosition = _lookAheadFactor * mousePosition.normalized * Mathf.Sign(deltas.Max());
+        }
+
+        Vector2 aheadTargetPos = (Vector2)trackingTarget.position + _lookAheadPosition;
+        aheadTargetPos = new Vector2(aheadTargetPos.x + _offsetX, aheadTargetPos.y + _offsetY);
+        Vector2 newPos = Vector2.SmoothDamp(transform.position, aheadTargetPos, ref _currentVelocity, _damping);
+
+        transform.position = new Vector3(newPos.x, Mathf.Clamp(newPos.y, _minHeight, _maxHeight), transform.position.z);
+
+        _lastTargetPosition = mousePosition;
+    }
+}

a1b3c7989daea11d1d07f1e799840c2d9fa3b1ca
Guy Chamberlain-Webber <chamberlainwebberj@gmail.com>
2022-10-01 11:52:22 +0100

    Added player rotation towards cursor

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -9,11 +9,22 @@ public class PlayerMovement : MonoBehaviour
 
     private void Start()
     {
-        _rb2d = GetComponent<Rigidbody2D>();      
+        _rb2d = GetComponent<Rigidbody2D>();
+        _rb2d.constraints = RigidbodyConstraints2D.FreezeRotation;
     }
 
     private void FixedUpdate()
     {
         _rb2d.velocity = new Vector2(Input.GetAxis("Horizontal") * moveSpeed, Input.GetAxis("Vertical") * moveSpeed);
-    }
+
+        Vector3 mousePosition = Input.mousePosition;
+        Vector3 playerScreenPos = Camera.main.WorldToScreenPoint(transform.position);
+
+        mousePosition.x -= playerScreenPos.x;
+        mousePosition.y -= playerScreenPos.y;
+
+        float angle = Mathf.Atan2(mousePosition.y, mousePosition.x) * Mathf.Rad2Deg;
+
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+     }
 }
